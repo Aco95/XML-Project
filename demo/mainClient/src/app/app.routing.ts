01ddc38136932311530df1d65a98b40d3ad0c563
Arsenije <dsd>
2018-06-12 20:43:18 +0200

    Kreiran glavni Client

---
+++ demo/mainClient/src/app/app.routing.ts
@@ -0,0 +1,26 @@
+import { ModuleWithProviders } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { HomePageComponent } from "./components/home-page/home-page.component";
+
+
+const appRoutes: Routes = 
+[
+    {
+        path: '',
+        redirectTo: '/homeSearch',
+        pathMatch:'full'
+        //component: LoginComponent
+    },
+
+    {
+        path:'homeSearch',
+        component: HomePageComponent
+    }
+
+]
+
+
+
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
