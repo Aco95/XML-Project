d6a36a3bddbbf22d880fcbd313dc28e88170cbd5
milan <milansuvajdzic24@gmail.com>
2018-06-14 20:00:53 +0200

    dd

--- demo/src/main/java/com/example/demo/controller/SmestajController.java
+++ demo/src/main/java/com/example/demo/controller/SmestajController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,20 +27,21 @@ public class SmestajController {
 			method = RequestMethod.GET, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Smestaj> getAll(){
-			
+		
+		System.out.println("Pogodio getAll!");
 		return smestajRepository.findAll();
 	}
 	
 	
-//	@RequestMapping(
-//			value = "/getAccommodationByPlace/{place}",
-//			method = RequestMethod.GET, 
-//			produces = MediaType.APPLICATION_JSON_VALUE)
-//	public List<Smestaj> getAccommodationByPlace(@PathVariable("place") String place){
-//		
-//		return smestajRepository.findByPlace(place);
-//		
-//	}
+	@RequestMapping(
+			value = "/getAccommodationByPlace/{place}",
+			method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Smestaj> getAccommodationByPlace(@PathVariable("place") String place){
+		
+		return smestajRepository.findByPlace(place);
+		
+	}
 	
 	
 	@RequestMapping(
@@ -49,11 +49,11 @@ public class SmestajController {
 			method = RequestMethod.GET, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Smestaj> basicSearch(@PathVariable("place") String place,
-			@PathVariable("numberOfPersons") int numberOfPersons,
+			@PathVariable("numberOfPersons") String numberOfPersons,
 			@PathVariable("dateFrom") String dateFrom, @PathVariable("dateTo") String dateTo){
 		
 		
-		
+		System.out.println("POGODJEN basic search");
 		// radim nestoo ... ..  .
 		
 		
