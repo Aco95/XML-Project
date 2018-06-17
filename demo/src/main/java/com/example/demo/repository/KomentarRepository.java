398d0a6adb47805b5a279deca25726ef25f9b8bc
sulepaja <m.s.zoric5@gmail.com>
2018-06-17 12:38:45 +0200

    Uradjeni komentari

--- demo/src/main/java/com/example/demo/repository/KomentarRepository.java
+++ demo/src/main/java/com/example/demo/repository/KomentarRepository.java
@@ -1,11 +1,17 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Komentar;
 
 @Repository
 public interface KomentarRepository extends MongoRepository<Komentar, String>{
-
+	
+	@Query("{'odobren' : ?0}")
+	List<Komentar> findByOdobren(Boolean odobren);
+	
 }
